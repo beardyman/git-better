e33233fee30907eb08b91b99f9d64296fa5ed20b
Beardyman <therealbeardyman@gmail.com>
2020-07-01 22:37:56 -0400

    git finish inital

---
+++ bin/git-finish.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const argv = require('minimist')(process.argv.slice(2));
+
+require('../src/finish')().then(()=>{
+  process.exit(0);
+})
---
+++ src/finish.js
@@ -0,0 +1,15 @@
+
+const utils = require('./utils');
+
+module.exports = async function finish() {
+  const branches = await git.branch();
+  const currentBranch = Branch.fromFullBranchName(branches.current);
+
+
+  // make sure the base branch is up to date
+  const baseBranch = utils.getBaseBranch(currentBranch);
+  await git.pull('origin', baseBranch.toString());
+
+  // merge the base branch into the current branch
+  await git.mergeFromTo(baseBranch.toString(), currentBranch.toString());
+}
